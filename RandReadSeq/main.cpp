b9cc5aeaf120166db03813558df6bb39e69a7a79
znik <nik.zaborovsky@gmail.com>
2014-08-12 16:34:12 -0700

    [MemBench] Improved the loop and the randomization
    * Improved the generation of random accesses within specified ranges
    * Increased accuracy by unrolling 256 iterations of the loop (loads VS
    branches)
    * Reduced the body loop to jumping to pointers

--- RandReadSeq/main.cpp
+++ RandReadSeq/main.cpp
@@ -14,9 +14,11 @@
 #include "stopwatch.h"
 
 
-#define RANDOM_RANGE
-#define NPAD_POWER	3
+#define NPAD_POWER	4
 #define NPAD ((1<<NPAD_POWER) - 1)
+#define TWICE(x)	x x
+#define TIMES_256(x)	TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(x))))))))
+
 
 static FILE *h_log;
 
@@ -42,34 +44,42 @@ void workCycle(const unsigned logSize, const unsigned logRangeElements) {
 	srand(time(0));
 
 	// Set elements pointers to random elements from the list
-	for (int i = 0; i < (1 << logSize); i += 1) {
-#ifdef RANDOM_RANGE
-		//std::vector<int> indices((1 << logRangeElements) - 1);
-		//for (unsigned d = 0; d < indices.size(); ++d) {
-		//	indices[d] = d;
-		//}
-		//std::shuffle(indices.begin(), indices.end(), std::default_random_engine(time(0)));
-		unsigned rand_range = i / (1 << logRangeElements);
-		// Select the range within there will be random accesses
-		cur->p = first + ((1 << logRangeElements) * rand_range) + rand() % (1 << logRangeElements);
-#else // RANDOM_RANGE
-		cur->p = first + rand() % (1 << logSize);
-#endif // RANDOM_RANGE
-		cur += 1;
+	for (int i = 0; i < (1 << (logSize - logRangeElements)); ++i) {
+		std::vector<int> indices((1 << logRangeElements) - 1);
+		for (unsigned d = 0; d < indices.size(); ++d) {
+			indices[d] = d + 1;
+		}
+
+		std::shuffle(indices.begin(), indices.end(), std::default_random_engine(time(0)));
+
+		if (indices.size() > 0 && 0 == indices[0])
+			std::swap(indices[0], indices[1]);
+
+		for (int b = 0; b < indices.size(); ++b) {
+			cur->p = first + ((1 << logRangeElements) * i) + indices[b];
+			cur = cur->p;
+		}
+		cur->p = first + ((1 << logRangeElements) * (i + 1));
+		cur = cur->p;
 	}
 
 	register int n, m;
+	register chunk_t* x = first->p;
+
 	swatch t;
 	t.reset();
 	for (n = 0; n < 128; ++n) {
-		for (m = 0; m < (1 << logSize); ++m) {
-			NO_OPTIMIZE((first + m)->p->p);
+		register int remain = (1 << logSize) / 256;
+		// Increase load instructions ratio in respect to branch instructions.
+		x = first->p;
+		while(remain--) {
+			TIMES_256(x = x->p;)
 		}
 	}
 	long long elapsed = t.get();
 	fprintf(h_log, "%d\t%d\t%d\t%d\n", logRangeElements,
 		sizeof(chunk_t) * (1 << logSize) / (1 << 10), 1 << logSize, 1000 * elapsed / (1 << logSize));
-	printf("RANGE: 2^%d, WS: %d KB, items: %d, time/128items: %d nanosec\n", logRangeElements,
+	printf("RANGE: 2^%d, WS: %d KB, items: %d, time/128items: %d nanosec\n", logRangeElements - 1,
 		sizeof(chunk_t) * (1 << logSize) / (1 << 10), 1 << logSize, 1000 * elapsed / (1 << logSize));
 
 	delete[] mem;
@@ -86,15 +96,9 @@ int main() {
 	printf("--- Chunk size: %u ---\n", sizeof(chunk_t));
 	fprintf(h_log, "--- Chunk size: %u ---\n", sizeof(chunk_t));
 	fprintf(h_log, "RANGE: power,\t WS: KB,\t items num,\t time/128items: picosec\n");
-#ifndef RANDOM_RANGE
-	for (unsigned i = 11 - NPAD_POWER; i < 29 - NPAD_POWER; ++i) {
-		workCycle(i, 0); // Second param is not used - stub
-	}
-#else
-	for (unsigned i = 3; i < 29 - NPAD_POWER; ++i) {
+	for (unsigned i = 0; i < 27 - NPAD_POWER; ++i) {
 		workCycle(26 - NPAD_POWER, i);
 	}
-#endif
 	fclose(h_log);
 	getchar();
 	return 1;
